5f49db7ec6c165b443530aed3ea6d33ffcfe53df
Salt Developer <salt-dev@Salts-MBP-6.localdomain>
2022-02-25 10:16:08 +0100

    add styling

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ const state = {
 const template = s => {
   if (s) {
     return s.pictures.map(pic => ` 
-        <div class="card flip-card">
-          <div class="card__figure flip-card-inner"> 
+        <section class="card flip-card">
+          <figure class="card__figure flip-card-inner"> 
             <div class="flip-card-front">
               <img class="card__img"  src="${pic.urls.regular}" alt="${pic.alt_description}">
               </div>
-            <div class="card__description flip-card-back">${pic.alt_description}</div>
-          </div>
-        </div>
+            <figcaption class="card__description flip-card-back">${pic.alt_description}</figcaption>
+          </figure>
+        </section>
         `).join('');
   }
   return '<span></span>';
